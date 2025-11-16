9a91e900d2ebd483a11238da2d479c465c6a0af0
peterpaulodev <peterpaulodev@gmail.com>
2025-11-16 14:05:33 -0300

    feat: add auth google with better auth

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,14 +1,53 @@
+"use client";
+
 import Image from "next/image";
 import { Button } from "./ui/button";
-import { MenuIcon } from "lucide-react";
+import { LogInIcon, LogOutIcon, MenuIcon } from "lucide-react";
+import { authClient } from "@/lib/auth-client";
+import { Avatar, AvatarImage } from "./ui/avatar";
 
 const Header = () => {
+  const { data: session } = authClient.useSession();
+  const handleLogin = async () => {
+    await authClient.signIn.social({
+      provider: "google",
+      callbackURL: "/",
+    });
+  };
+
+  const handleLogout = async () => {
+    await authClient.signOut();
+  };
+
   return (
     <header className="flex items-center justify-between bg-white px-5 py-6">
       <Image src="/logo.svg" alt="Logo" width={100} height={26.09} />
-      <Button variant="outline" size="icon">
-        <MenuIcon />
-      </Button>
+      <div className="flex items-center gap-2">
+        {session ? (
+          <>
+            <Avatar>
+              <AvatarImage
+                src={session.user.image || ""}
+                alt={session.user.name || "User"}
+              />
+            </Avatar>
+            <span className="text-foreground text-sm">
+              Olá, {session.user.name}
+            </span>
+            <Button variant="outline" size="icon" onClick={handleLogout}>
+              <LogOutIcon />
+            </Button>
+          </>
+        ) : (
+          <Button variant="outline" size="icon" onClick={handleLogin}>
+            <LogInIcon />
+          </Button>
+        )}
+
+        <Button variant="outline" size="icon">
+          <MenuIcon />
+        </Button>
+      </div>
     </header>
   );
 };
